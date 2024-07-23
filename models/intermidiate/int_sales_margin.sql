90787cc141e3e2a8e05a9f6f579ac3c1589b46e4
anth.girbes@gmail.com <anth.girbes@gmail.com>
2024-07-23 12:43:55 +0000

    margin and puchase cost addition

---
+++ models/intermidiate/int_sales_margin.sql
@@ -0,0 +1,11 @@
+SELECT 
+    sales.date_date
+    ,sales.products_id
+    ,sales.revenue
+    ,sales.quantity
+    ,product.purchase_price
+    ,CAST(product.purchase_price AS FLOAT64)*(sales.quantity) AS purchase_cost
+    ,sales.revenue-CAST(product.purchase_price AS FLOAT64)*(sales.quantity) AS margin
+FROM {{ ref('stg_raw__sales') }} AS sales
+JOIN {{ ref('stg_raw__product') }} AS product
+ON product.products_id = sales.products_id
