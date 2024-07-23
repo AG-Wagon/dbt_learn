642972bcd7b3a212d1bfea447bef5b98711dc5d6
anth.girbes@gmail.com <anth.girbes@gmail.com>
2024-07-23 13:22:13 +0000

    shipping rip off

---
+++ models/intermidiate/int_orders_operational.sql
@@ -0,0 +1,7 @@
+SELECT
+    orders_id
+    ,date_date
+    ,margin+shipping_fee-logcost-CAST(ship_cost AS FLOAT64) AS operational_margin
+FROM {{ ref('int_orders_margin') }}
+JOIN {{ ref('stg_raw__ship') }}
+USING (orders_id)
